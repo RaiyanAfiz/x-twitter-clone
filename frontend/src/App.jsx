881e7fc13bbf563c7cb4b0b6ae7600e7156237ea
Raiyan Afiz <raiyan.afiz@gmail.com>
2025-04-23 10:12:51 -0400

    Add pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import NotFoundPage from "./pages/not_found/NotFoundPage";
+
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
@@ -7,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </div>
   );
