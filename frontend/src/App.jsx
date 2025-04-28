d79b7bb4cd8f538d937d776368b7529f19423d1a
Raiyan Afiz <raiyan.afiz@gmail.com>
2025-04-28 16:36:03 -0400

    Adds finished front end

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,16 +4,26 @@ import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
 import NotFoundPage from "./pages/not_found/NotFoundPage";
+import NotificationPage from "./pages/notification/NotificationPage";
+import ProfilePage from "./pages/profile/ProfilePage";
+
+import Sidebar from "./components/common/Sidebar";
+import RightPanel from "./components/common/RightPanel";
 
 function App() {
   return (
     <div className="flex max-w-6xl mx-auto">
+      {/* This is a common component since isn't inside routes*/}
+      <Sidebar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/notifications" element={<NotificationPage />} />
+        <Route path="/profile/:username" element={<ProfilePage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
+      <RightPanel />
     </div>
   );
 }
