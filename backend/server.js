2233a1db9ba330f4940bc234c1f0e880baa5706c
Raiyan Afiz <raiyan.afiz@gmail.com>
2025-05-01 18:31:30 -0400

    Adds allows image to be added to posts

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(express.json()); // to parse req.body
+app.use(express.json({ limit: "5mb" })); // to parse req.body
 app.use(express.urlencoded({ extended: true })); // to parse postman form data (urlencoded)
 app.use(cookieParser());
 
