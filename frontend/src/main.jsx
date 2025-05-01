a2a08420115f7e91f84abe381641afab4141f7af
Raiyan Afiz <raiyan.afiz@gmail.com>
2025-04-30 22:39:33 -0400

    Connects FE & BE for auth

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,13 +3,22 @@ import { createRoot } from "react-dom/client";
 import "./main.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { refetchOnWindowFocus: false },
+  },
+});
 
 // This is not the best way to use react router
 // Redo it in the modern way
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
